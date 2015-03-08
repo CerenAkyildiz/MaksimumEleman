92e4c66355b256b2858cea67789077145f8d1fbe
Ceren <akyildiz_ceren@hotmail.com>
2015-03-09 00:26:54 +0200

    MaxEleman
    Dizinn max elemanını bulma

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaksimumEleman
+{
+    class Program
+    {
+
+        public static int MaxBul(int [] dizi )
+        {
+            int enbuyukEleman = 0;
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                if (dizi[i] > enbuyukEleman)
+                {
+                    enbuyukEleman = dizi[i];
+                }
+
+            }
+            return enbuyukEleman;
+        }
+        static void Main(string[] args)
+        {
+
+              int[] diziK = { 5, 3, 26, -1, 10, 25, 3, 4 };
+     
+            
+         Console.WriteLine("En büyük eleman: "+MaxBul(diziK));
+
+         Console.ReadLine();
+
+        }
+    }
+}
+
+   
